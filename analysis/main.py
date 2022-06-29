005d831a478ba0f8017460808953da50e03dd0cf
kravitsjacob <kravitsjacob@gmail.com>
2022-06-28 18:17:44 -0600

    Call once through withdrawal from main

--- analysis/main.py
+++ analysis/main.py
@@ -2,5 +2,15 @@
 
 import wc
 
-a = 1
 
+def main():
+    wc.once_through_withdrawal(
+        eta_net=0.25,
+        k_os=0.25,
+        delta_t=5,
+        beta_proc=200
+    )
+
+
+if __name__ == '__main__':
+    main()
