5e5047d02bb6ca1ebea34707debfc9a6e878c54a
kravitsjacob <kravitsjacob@gmail.com>
2022-06-30 21:37:43 -0600

    Create test for recirculating model

--- testing/test.py
+++ testing/test.py
@@ -17,6 +17,20 @@ class CoreTest(unittest.TestCase):
         )
         self.assertAlmostEquals(beta_with, 34616, -2)
 
+    def test_reciruclating_withdrawal(self):
+        """
+        Test for reciruclating_withdrawal
+        """
+        beta_with = wc.recirculating_withdrawal(
+            eta_net=0.20,
+            k_os=0.25,
+            delta_t=5,
+            beta_proc=200,
+            eta_cc=5,
+            k_evap=0.15
+        )
+        self.assertAlmostEquals(beta_with, 9073, -2)
+
 
 if __name__ == '__main__':
     unittest.main()
