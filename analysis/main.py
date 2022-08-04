73a74f7c07f275664baf052c0c8e03a92b109495
kravitsjacob <kravitsjacob@gmail.com>
2022-08-04 11:19:03 -0600

    Add cooling systems to synthetic grid

--- analysis/main.py
+++ analysis/main.py
@@ -1,4 +1,4 @@
-"""Main analysis script"""
+"""Preprocessing python script"""
 
 
 import pandapower
@@ -10,7 +10,6 @@ import wc
 
 
 def main():
-    # Inputs
     with open('analysis/paths.yml', 'r') as f:
         paths = yaml.safe_load(f)
 
@@ -30,7 +29,7 @@ def main():
         pandapower.to_pickle(net, paths['outputs']['case_match'])
         print('Success: generator_match')
 
-    # Synthetic grid cooling system information
+    # Add cooling systems to synthetic grid
     if not os.path.exists(paths['outputs']['gen_info_water']):
         df_eia = wc.core.import_eia(paths['inputs']['eia_raw'])
         df_eia.to_hdf(paths['outputs']['eia'], key='df_eia', mode='w')
@@ -40,28 +39,6 @@ def main():
         df_gen_info_water = wc.core.get_cooling_system(df_eia, df_gen_info)
         df_gen_info_water.to_csv(paths['outputs']['gen_info_water'])
         print('Success: get_cooling_system')
-
-    # Power simulation simulation inputs
-    if not os.path.exists(paths['outputs']['loads']):
-        df_load = pd.read_csv(paths['outputs']['df_load'])
-        fig = wc.loads(df_load)
-        fig.savefig(paths['outputs']['loads'])
-
-    # Power simulation simulation no ramping limits
-    if not os.path.exists(paths['outputs']['gen_noramp']):
-        df_gen_info_water = pd.read_csv(paths['outputs']['gen_info_water'])
-        df_gen_pminfo = pd.read_csv(paths['outputs']['df_gen_pminfo'])
-        df_gen = pd.read_csv(paths['outputs']['df_gen_noramp'])
-        g = wc.gen_timeseries(df_gen, df_gen_pminfo, df_gen_info_water)
-        g.savefig(paths['outputs']['gen_noramp'])
-
-    # Power simulation simulation with ramping limits
-    if not os.path.exists(paths['outputs']['gen_ramp']):
-        df_gen_info_water = pd.read_csv(paths['outputs']['gen_info_water'])
-        df_gen_pminfo = pd.read_csv(paths['outputs']['df_gen_pminfo'])
-        df_gen = pd.read_csv(paths['outputs']['df_gen_ramp'])
-        g = wc.gen_timeseries(df_gen, df_gen_pminfo, df_gen_info_water)
-        g.savefig(paths['outputs']['gen_ramp'])
 
 
 if __name__ == '__main__':
