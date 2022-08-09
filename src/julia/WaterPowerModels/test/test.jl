4c3200a51edfee5ca562b47a32b0d81d64de95d1
kravitsjacob <kravitsjacob@gmail.com>
2022-08-09 16:44:32 -0600

    Test for recirculating withdrawal

--- src/julia/WaterPowerModels/test/test.jl
+++ src/julia/WaterPowerModels/test/test.jl
@@ -27,28 +27,25 @@ function test_once_through_consumption()
     )
 end
 
-
-
-# function test_reciruclating_withdrawal()
-#     """
-#     Test for test_reciruclating_withdrawal
-#     """
-#     beta_with = WaterPowerModels.recirculating_withdrawal(
-#         eta_net=0.20,
-#         k_os=0.25,
-#         beta_proc=200,
-#         eta_cc=5.0,
-#         k_sens=0.15
-#     )
-#     @Test.test isapprox(beta_with, 4486, atol=1 )
-# end
+function test_reciruclating_withdrawal()
+    """
+    Test for test_reciruclating_withdrawal
+    """
+    beta_with = WaterPowerModels.recirculating_withdrawal(
+        eta_net=0.20,
+        k_os=0.25,
+        beta_proc=200.0,
+        eta_cc=5,
+        k_sens=0.15
+    )
+end
 
 
 function main()
-    @Test.testset "Water Models" begin
-        # Water use models
-        @Test.test isapprox(test_once_through_withdrawal(), 34616.0, atol=1 )
-        @Test.test isapprox(test_once_through_consumption(), 544.0, atol=1 )
+    @Test.testset "Water Use Models" begin
+        @Test.test isapprox(test_once_through_withdrawal(), 34616.0, atol=1)
+        @Test.test isapprox(test_once_through_consumption(), 544.0, atol=1)
+        @Test.test isapprox(test_reciruclating_withdrawal(), 4486.0, atol=1)
     end
 end
 
