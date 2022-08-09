5899f92583222cb0e971c4662864307e7926c5ae
kravitsjacob <kravitsjacob@gmail.com>
2022-08-09 16:58:14 -0600

    Make test for test_recirculating_consumption

--- src/julia/WaterPowerModels/test/test.jl
+++ src/julia/WaterPowerModels/test/test.jl
@@ -40,12 +40,25 @@ function test_reciruclating_withdrawal()
     )
 end
 
+function test_recirculating_consumption()
+    """
+    Test for recirculating_consumption
+    """
+    beta_con = WaterPowerModels.recirculating_consumption(
+        eta_net=0.20,
+        k_os=0.25,
+        beta_proc=200.0,
+        eta_cc=5,
+        k_sens=0.15
+    )
+end
 
 function main()
     @Test.testset "Water Use Models" begin
         @Test.test isapprox(test_once_through_withdrawal(), 34616.0, atol=1)
         @Test.test isapprox(test_once_through_consumption(), 544.0, atol=1)
         @Test.test isapprox(test_reciruclating_withdrawal(), 4486.0, atol=1)
+        @Test.test isapprox(test_recirculating_consumption(), 3629.0, atol=1)
     end
 end
 
