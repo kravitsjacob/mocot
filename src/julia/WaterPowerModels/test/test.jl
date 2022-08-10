466b39050feaac38b2311611632779b736f3ff1b
kravitsjacob <kravitsjacob@gmail.com>
2022-08-09 21:45:02 -0600

    Create test for faily water use model

--- src/julia/WaterPowerModels/test/test.jl
+++ src/julia/WaterPowerModels/test/test.jl
@@ -1,5 +1,8 @@
 using Revise
 using Test
+using DataFrames
+using XLSX
+using Infiltrator
 using WaterPowerModels
 
 
@@ -54,12 +57,25 @@ function test_recirculating_consumption()
 end
 
 function main()
-    @Test.testset "Water Use Models" begin
+    @Test.testset "Fundamental Water Use Models" begin
         @Test.test isapprox(test_once_through_withdrawal(), 34616.0, atol=1)
         @Test.test isapprox(test_once_through_consumption(), 544.0, atol=1)
         @Test.test isapprox(test_reciruclating_withdrawal(), 4486.0, atol=1)
         @Test.test isapprox(test_recirculating_consumption(), 3629.0, atol=1)
     end
+
+    @Test.testset "Test for daily_water_use" begin
+        pg = 1.0
+        exogenous_dict = Dict("air_temperature"=>25.0, "water_temperature"=>25.0)
+        gen_info_dict = Dict("fuel_type"=>"coal", "cool"=>"OC")
+        df_eia_heat_rates = DataFrames.DataFrame(XLSX.readtable("analysis/io/inputs/eia_heat_rates/Table_A6_Approximate_Heat_Rates_for_Electricity_-_and_Heat_Content_of_Electricity.xlsx", "Annual Data"))
+
+        beta_with, beta_con = WaterPowerModels.daily_water_use(pg, exogenous_dict, gen_info_dict, df_eia_heat_rates)
+        @Test.test isapprox(beta_with, 20992, atol=1)
+        @Test.test isapprox(beta_con, 407, atol=1)
+    end
+
+    test_daily_water_use()
 end
 
 main()
