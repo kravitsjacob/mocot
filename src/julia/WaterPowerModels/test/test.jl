173e944ad4fb9478f9230afc02cced34165318ee
kravitsjacob <kravitsjacob@gmail.com>
2022-08-09 22:00:04 -0600

    Refactor testing scripts

--- src/julia/WaterPowerModels/test/test.jl
+++ src/julia/WaterPowerModels/test/test.jl
@@ -6,34 +6,21 @@ using Infiltrator
 using WaterPowerModels
 
 
-function test_once_through_withdrawal()
-    """
-    Test for once_through_withdrawal
-    """
+@Test.testset "Fundamental Water Use Models" begin
     beta_with = WaterPowerModels.once_through_withdrawal(
         eta_net=0.25,
         k_os=0.25,
         delta_t=5.0,
         beta_proc=200.0
     )
-end
-
-function test_once_through_consumption()
-    """
-    Test for once_through_consumption
-    """
+    @Test.test isapprox(beta_with, 34616.0, atol=1)
     beta_con = WaterPowerModels.once_through_consumption(
         eta_net=0.25,
         k_os=0.25,
         delta_t=5.0,
         beta_proc=200.0
     )
-end
-
-function test_reciruclating_withdrawal()
-    """
-    Test for test_reciruclating_withdrawal
-    """
+    @Test.test isapprox(beta_con, 544.0, atol=1)
     beta_with = WaterPowerModels.recirculating_withdrawal(
         eta_net=0.20,
         k_os=0.25,
@@ -41,12 +28,7 @@ function test_reciruclating_withdrawal()
         eta_cc=5,
         k_sens=0.15
     )
-end
-
-function test_recirculating_consumption()
-    """
-    Test for recirculating_consumption
-    """
+    @Test.test isapprox(beta_with, 4486.0, atol=1)
     beta_con = WaterPowerModels.recirculating_consumption(
         eta_net=0.20,
         k_os=0.25,
@@ -54,36 +36,23 @@ function test_recirculating_consumption()
         eta_cc=5,
         k_sens=0.15
     )
+    @Test.test isapprox(beta_con, 3629.0, atol=1)
 end
 
-function main()
-    @Test.testset "Fundamental Water Use Models" begin
-        @Test.test isapprox(test_once_through_withdrawal(), 34616.0, atol=1)
-        @Test.test isapprox(test_once_through_consumption(), 544.0, atol=1)
-        @Test.test isapprox(test_reciruclating_withdrawal(), 4486.0, atol=1)
-        @Test.test isapprox(test_recirculating_consumption(), 3629.0, atol=1)
-    end
+@Test.testset "Test for daily_water_use" begin
+pg = 1.0
+df_eia_heat_rates = DataFrames.DataFrame(XLSX.readtable("analysis/io/inputs/eia_heat_rates/Table_A6_Approximate_Heat_Rates_for_Electricity_-_and_Heat_Content_of_Electricity.xlsx", "Annual Data"))
+exogenous_dict = Dict("air_temperature"=>25.0, "water_temperature"=>25.0)
 
-    @Test.testset "Test for daily_water_use" begin
-        pg = 1.0
-        df_eia_heat_rates = DataFrames.DataFrame(XLSX.readtable("analysis/io/inputs/eia_heat_rates/Table_A6_Approximate_Heat_Rates_for_Electricity_-_and_Heat_Content_of_Electricity.xlsx", "Annual Data"))
-        exogenous_dict = Dict("air_temperature"=>25.0, "water_temperature"=>25.0)
+# Once-through coal
+gen_info_dict = Dict("fuel_type"=>"coal", "cool"=>"OC")
+beta_with, beta_con = WaterPowerModels.daily_water_use(pg, exogenous_dict, gen_info_dict, df_eia_heat_rates)
+@Test.test isapprox(beta_with, 20992, atol=1)
+@Test.test isapprox(beta_con, 407, atol=1)
 
-        # Once-through coal
-        gen_info_dict = Dict("fuel_type"=>"coal", "cool"=>"OC")
-        beta_with, beta_con = WaterPowerModels.daily_water_use(pg, exogenous_dict, gen_info_dict, df_eia_heat_rates)
-        @Test.test isapprox(beta_with, 20992, atol=1)
-        @Test.test isapprox(beta_con, 407, atol=1)
-
-        # Recirculating coal
-        gen_info_dict = Dict("fuel_type"=>"nuclear", "cool"=>"RC")
-        beta_with, beta_con = WaterPowerModels.daily_water_use(pg, exogenous_dict, gen_info_dict, df_eia_heat_rates)
-        @infiltrate
-        @Test.test isapprox(beta_with, 3290.0, atol=1)
-        @Test.test isapprox(beta_con, 2634.0, atol=1)
-    end
-
-    test_daily_water_use()
+# Recirculating coal
+gen_info_dict = Dict("fuel_type"=>"nuclear", "cool"=>"RC")
+beta_with, beta_con = WaterPowerModels.daily_water_use(pg, exogenous_dict, gen_info_dict, df_eia_heat_rates)
+@Test.test isapprox(beta_with, 3290.0, atol=1)
+@Test.test isapprox(beta_con, 2634.0, atol=1)
 end
-
-main()
