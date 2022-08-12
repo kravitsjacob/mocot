e7fd3478ce5173e70a6a0d557df3baf3cc7c2b58
kravitsjacob <kravitsjacob@gmail.com>
2022-08-12 10:04:33 -0600

    Move simulation to function

--- src/julia/WaterPowerModels/src/WaterPowerModels.jl
+++ src/julia/WaterPowerModels/src/WaterPowerModels.jl
@@ -4,6 +4,7 @@ import PowerModels
 import JuMP
 import DataFrames
 import Statistics
+import Ipopt
 import Infiltrator  # For debugging only
 
 
@@ -468,6 +469,149 @@ function set_all_gens!(nw_data, prop:: String, val)
         gen_dict[prop] = val
     end
     return nw_data
+end
+
+function simulation(
+    df_gen_info_water:: DataFrames.DataFrame, 
+    df_eia_heat_rates:: DataFrames.DataFrame, 
+    df_air_water:: DataFrames.DataFrame,
+    df_node_load:: DataFrames.DataFrame,
+    network_data:: Dict,
+    ;
+    w_with:: Float64=0.0,
+    w_con:: Float64=0.0,
+)
+    """
+    Simulation of water and energy system
+
+    # Arguments
+    - `df_gen_info_water:: DataFrames.DataFrame`: Generator information with water
+    - `df_eia_heat_rates:: DataFrames.DataFrame`: EIA heat rates
+    - `df_air_water:: DataFrames.DataFrame`: Exogenous air and water temperatures
+    - `df_node_load:: DataFrames.DataFrame`: Node-level loads
+    - `network_data:: Dict`: PowerModels Network data
+    - `w_with:: Float64=0.0`: Withdrawal weight
+    - `w_con:: Float64=0.0`: Consumption weight
+    """
+    # Initialization
+    power_results = Dict{String, Dict}()
+    with_results = Dict{String, Dict}()
+    con_results = Dict{String, Dict}()
+
+    # Import static network
+    h_total = 24
+    d_total = 7
+
+    # Commit all generators
+    network_data = WaterPowerModels.set_all_gens!(network_data, "gen_status", 1)
+    network_data = WaterPowerModels.set_all_gens!(network_data, "pmin", 0.0)
+    network_data_multi = PowerModels.replicate(network_data, h_total)
+
+    # Static network information
+    df_gen_info_pm = WaterPowerModels.network_to_df(network_data, "gen", ["gen_bus"])
+    df_gen_info = DataFrames.leftjoin(
+        df_gen_info_pm,
+        df_gen_info_water,
+        on = :gen_bus => Symbol("MATPOWER Index")
+    )
+
+    # Initialize water use based on 25.0 C
+    d = 0
+    gen_beta_with = Dict{String, Float64}()
+    gen_beta_con = Dict{String, Float64}()
+    water_temperature = 25.0
+    air_temperature = 25.0
+    for row in DataFrames.eachrow(df_gen_info)
+        gen_name = row["obj_name"]
+        fuel =  string(row["MATPOWER Fuel"])
+        cool = string(row["923 Cooling Type"])
+        beta_with, beta_con = WaterPowerModels.daily_water_use(
+            water_temperature,
+            air_temperature,
+            fuel,
+            cool,
+            df_eia_heat_rates
+        )
+        gen_beta_with[gen_name] = beta_with
+        gen_beta_con[gen_name] = beta_con   
+    end
+    with_results[string(d)] = gen_beta_with
+    con_results[string(d)] = gen_beta_con
+
+    # Simulation
+    for d in 1:d_total
+        # Update loads
+        network_data_multi = WaterPowerModels.update_load!(
+            network_data_multi,
+            df_node_load,
+            d
+        )
+
+        # Create power system model
+        pm = PowerModels.instantiate_model(
+            network_data_multi,
+            PowerModels.DCPPowerModel,
+            PowerModels.build_mn_opf
+        )
+
+        # Add water use penalities
+        pm = WaterPowerModels.add_water_terms!(
+            pm,
+            with_results[string(d-1)],
+            w_with
+        )
+        pm = WaterPowerModels.add_water_terms!(
+            pm,
+            con_results[string(d-1)],
+            w_con
+        )
+
+        # Solve power system model
+        day_results = PowerModels.optimize_model!(pm, optimizer=Ipopt.Optimizer)
+        
+        # Group generators
+        df_gen_pg = WaterPowerModels.multi_network_to_df(
+            day_results["solution"]["nw"],
+            "gen",
+            ["pg"]
+        )
+        df_gen_pg = DataFrames.combine(
+            DataFrames.groupby(df_gen_pg, :obj_name),
+            :pg => Statistics.mean
+        )
+
+        # Exogenous air and water temperatures
+        d_pp = d - 1
+        air_water = df_air_water[in([d_pp]).(df_air_water.Column1), :]
+        air_temperature = air_water[!, "air_temperature"][1]
+        water_temperature = air_water[!, "water_temperature"][1]
+
+        # Water use
+        gen_beta_with = Dict{String, Float64}()
+        gen_beta_con = Dict{String, Float64}()
+        for row in DataFrames.eachrow(df_gen_pg)
+            # Get generator information
+            gen_name = row["obj_name"]
+            gen_info = df_gen_info[in([gen_name]).(df_gen_info.obj_name), :]
+            fuel = string(gen_info[!, "MATPOWER Fuel"][1])
+            cool = string(gen_info[!, "923 Cooling Type"][1])
+            beta_with, beta_con = WaterPowerModels.daily_water_use(
+                water_temperature,
+                air_temperature,
+                fuel,
+                cool,
+                df_eia_heat_rates
+            )
+            gen_beta_with[gen_name] = beta_with
+            gen_beta_con[gen_name] = beta_con   
+        end
+
+        # Store results for that day
+        power_results[string(d)] = day_results
+        with_results[string(d)] = gen_beta_with
+        con_results[string(d)] = gen_beta_con
+    end
+    return power_results, with_results, con_results, df_gen_info_pm
 end
 
 end # module
