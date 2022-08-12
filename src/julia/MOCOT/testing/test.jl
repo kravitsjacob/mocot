ee86216112ececcfca9b840be77a3df28e285b3a
kravitsjacob <kravitsjacob@gmail.com>
2022-08-12 11:31:53 -0600

    Update testing for julia package

--- src/julia/MOCOT/testing/test.jl
+++ src/julia/MOCOT/testing/test.jl
@@ -1,31 +1,30 @@
-using Revise
 using Test
 using DataFrames
-using XLSX
-using Infiltrator
-using WaterPowerModels
 using YAML
+using XLSX
 using PowerModels
 using JuMP
+
+using MOCOT
 
 paths = YAML.load_file("analysis/paths.yml")
 
 @Test.testset "Fundamental Water Use Models" begin
-    beta_with = WaterPowerModels.once_through_withdrawal(
+    beta_with = MOCOT.once_through_withdrawal(
         eta_net=0.25,
         k_os=0.25,
         delta_t=5.0,
         beta_proc=200.0
     )
     @Test.test isapprox(beta_with, 34616.0, atol=1)
-    beta_con = WaterPowerModels.once_through_consumption(
+    beta_con = MOCOT.once_through_consumption(
         eta_net=0.25,
         k_os=0.25,
         delta_t=5.0,
         beta_proc=200.0
     )
     @Test.test isapprox(beta_con, 544.0, atol=1)
-    beta_with = WaterPowerModels.recirculating_withdrawal(
+    beta_with = MOCOT.recirculating_withdrawal(
         eta_net=0.20,
         k_os=0.25,
         beta_proc=200.0,
@@ -33,7 +32,7 @@ paths = YAML.load_file("analysis/paths.yml")
         k_sens=0.15
     )
     @Test.test isapprox(beta_with, 4486.0, atol=1)
-    beta_con = WaterPowerModels.recirculating_consumption(
+    beta_con = MOCOT.recirculating_consumption(
         eta_net=0.20,
         k_os=0.25,
         beta_proc=200.0,
@@ -51,21 +50,21 @@ end
     # Once-through coal
     fuel = "coal"
     cool = "OC"
-    beta_with, beta_con = WaterPowerModels.daily_water_use(air_temperature, water_temperature, fuel, cool, df_eia_heat_rates)
+    beta_with, beta_con = MOCOT.daily_water_use(air_temperature, water_temperature, fuel, cool, df_eia_heat_rates)
     @Test.test isapprox(beta_with, 20992, atol=1)
     @Test.test isapprox(beta_con, 407, atol=1)
 
     # Recirculating coal
     fuel = "coal"
     cool = "RC"
-    beta_with, beta_con = WaterPowerModels.daily_water_use(air_temperature, water_temperature, fuel, cool, df_eia_heat_rates)
+    beta_with, beta_con = MOCOT.daily_water_use(air_temperature, water_temperature, fuel, cool, df_eia_heat_rates)
     @Test.test isapprox(beta_with, 2855.0, atol=1)
     @Test.test isapprox(beta_con, 2324.0, atol=1)
 
     # Recirculating nuclear
     fuel = "nuclear"
     cool = "RC"
-    beta_with, beta_con = WaterPowerModels.daily_water_use(air_temperature, water_temperature, fuel, cool, df_eia_heat_rates)
+    beta_with, beta_con = MOCOT.daily_water_use(air_temperature, water_temperature, fuel, cool, df_eia_heat_rates)
     @Test.test isapprox(beta_with, 3290.0, atol=1)
     @Test.test isapprox(beta_con, 2634.0, atol=1)
 end
@@ -91,7 +90,7 @@ end
     )
 
     # Add water terms
-    pm = WaterPowerModels.add_water_terms!(
+    pm = MOCOT.add_water_terms!(
         pm,
         beta_dict,
         w
