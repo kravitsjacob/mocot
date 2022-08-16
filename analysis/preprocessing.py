336698082f639942e08d4305722e9ce2215d4090
kravitsjacob <kravitsjacob@gmail.com>
2022-08-16 16:25:17 -0600

    major cleanup preprocessing script

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -13,54 +13,64 @@ def main():
     with open('analysis/paths.yml', 'r') as f:
         paths = yaml.safe_load(f)
 
-    # Add generator information from MATPOWER to pandapower network
-    if not os.path.exists(paths['outputs']['case']):
-        net = pandapower.converter.from_mpc(paths['inputs']['case'])
-        df_gen_info = pd.read_csv(paths['inputs']['gen_info'])
-        net = mocot.core.grid_setup(net, df_gen_info)
-        pandapower.to_pickle(net, paths['outputs']['case'])
-        print('Success: grid_setup')
-
-    # Manual synthetic to real generator matching
-    if not os.path.exists(paths['outputs']['case_match']):
-        net = pandapower.from_pickle(paths['outputs']['case'])
-        df_gen_matches = pd.read_csv(paths['inputs']['gen_matches'])
-        net = mocot.core.generator_match(net, df_gen_matches)
-        pandapower.to_pickle(net, paths['outputs']['case_match'])
-        print('Success: generator_match')
-
-    # Add cooling systems to synthetic grid
+    # Generator cooling systems information
     if not os.path.exists(paths['outputs']['gen_info_water']):
+        # Generator information from previous study
+        df_gen_info_matpower = pd.read_csv(paths['inputs']['gen_info_matpower'])
+        df_gen_info_matches = pd.read_csv(paths['inputs']['gen_info_matches'])
+        df_gen_info = pd.merge(df_gen_info_matpower, df_gen_info_matches)
+
+        # Import EIA data
         df_eia = mocot.core.import_eia(paths['inputs']['eia_raw'])
-        df_eia.to_hdf(paths['outputs']['eia'], key='df_eia', mode='w')
         print('Success: import_eia')
-        net = pandapower.from_pickle(paths['outputs']['case_match'])
-        df_gen_info = mocot.core.network_to_gen_info(net)
+
+        # Add cooling system information
         df_gen_info_water = mocot.core.get_cooling_system(df_eia, df_gen_info)
-        df_gen_info_water.to_csv(paths['outputs']['gen_info_water'])
+        df_gen_info_water.to_csv(
+            paths['outputs']['gen_info_water'],
+            index=False
+        )
         print('Success: get_cooling_system')
 
+    # Add ramping information
+    if not os.path.exists(paths['outputs']['gen_info_water_ramp']):
+        df_gen_info_ramp = pd.read_excel(paths['inputs']['gen_info_ramp'])
+        df_gen_info_water = pd.read_csv(paths['outputs']['gen_info_water'])
+        df_gen_info_water_ramp = pd.merge(
+            left=df_gen_info_water,
+            right=df_gen_info_ramp
+        )
+        drop_cols = [
+            'Match Type',
+            'Source'
+        ]
+        df_gen_info_water_ramp = df_gen_info_water_ramp.drop(drop_cols, axis=1)
+        df_gen_info_water_ramp.to_csv(
+            paths['outputs']['gen_info_water_ramp'],
+            index=False
+        )
+        print('Success: Adding ramping information')
+
     # Water and air temperature
-    if not os.path.exists(paths['outputs']['df_air_water']):
+    if not os.path.exists(paths['outputs']['air_water']):
         df_air_water = mocot.core.process_exogenous(paths)
-        df_air_water.to_csv(paths['outputs']['df_air_water'])
+        df_air_water.to_csv(paths['outputs']['air_water'], index=False)
 
     # System-level loads
-    if not os.path.exists(paths['outputs']['df_system_load']):
+    if not os.path.exists(paths['outputs']['system_load']):
         df_miso = pd.read_csv(paths['inputs']['miso_load'])
         df_system_load = mocot.core.clean_system_load(df_miso)
-        df_system_load.to_csv(paths['outputs']['df_system_load'])
+        df_system_load.to_csv(paths['outputs']['system_load'], index=False)
 
     # Node-level loads
-    if not os.path.exists(paths['outputs']['df_node_load']):
-        net = pandapower.from_pickle(paths['outputs']['case_match'])
-        df_system_load = pd.read_csv(
-            paths['outputs']['df_system_load'],
-            index_col=0
+    if not os.path.exists(paths['outputs']['node_load']):
+        net = pandapower.converter.from_mpc(paths['inputs']['case'])
+        df_system_load = pd.read_csv(paths['outputs']['system_load'])
+        df_miso_load = pd.read_csv(paths['inputs']['miso_load'])
+        df_node_load = mocot.core.create_node_load(
+            df_system_load, df_miso_load, net
         )
-        df_miso = pd.read_csv(paths['inputs']['miso_load'])
-        df_node_load = mocot.core.create_node_load(df_system_load, df_miso, net)
-        df_node_load.to_csv(paths['outputs']['df_node_load'])
+        df_node_load.to_csv(paths['outputs']['node_load'], index=False)
 
 
 if __name__ == '__main__':
