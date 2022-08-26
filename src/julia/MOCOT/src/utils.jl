73cdcfce8e73ece58277c2039ca22cee40cb1eae
kravitsjacob <kravitsjacob@gmail.com>
2022-08-26 15:24:41 -0600

    Move set_all_gens!

---
+++ src/julia/MOCOT/src/utils.jl
@@ -0,0 +1,16 @@
+# Utilities
+
+function set_all_gens!(nw_data, prop:: String, val)
+    """
+    Change property on all generators in a network
+
+    # Arguments
+    - `nw_data::Dict`: Network data
+    - `prop:: String`: Generator property name
+    - `val`: Value to set
+    """
+    for gen_dict in values(nw_data["gen"])
+        gen_dict[prop] = val
+    end
+    return nw_data
+end
