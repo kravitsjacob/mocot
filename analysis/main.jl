90ec7716cd1bb5ec512ccc0029262a770605c653
kravitsjacob <kravitsjacob@gmail.com>
2022-08-26 18:22:08 -0600

    Create simulation config file

--- analysis/main.jl
+++ analysis/main.jl
@@ -25,43 +25,32 @@ function main()
         paths["inputs"]["case"]
     )
 
-    # Simulation with no water weights
-    (objectives, state) = MOCOT.simulation(
-        network_data,
-        df_gen_info, 
-        df_eia_heat_rates, 
-        df_air_water,
-        df_node_load,
-        w_with_coal=0.0,
-        w_con_coal=0.0,
-        w_with_ng=0.0,
-        w_con_ng=0.0,
-        w_with_nuc=0.0,
-        w_con_nuc=0.0,
-    )
+    # Simulation
+    df_config = DataFrames.DataFrame(CSV.File(paths["inputs"]["simulation_config"]))
+    df_objs = DataFrames.DataFrame()
+    for row in DataFrames.eachrow(df_config[1:2,:])
+        (objectives, state) = MOCOT.simulation(
+            network_data,
+            df_gen_info, 
+            df_eia_heat_rates, 
+            df_air_water,
+            df_node_load,
+            w_with_coal=row["w_with_coal"],
+            w_con_coal=row["w_con_coal"],
+            w_with_ng=row["w_with_ng"],
+            w_con_ng=row["w_con_ng"],
+            w_with_nuc=row["w_with_nuc"],
+            w_con_nuc=row["w_con_nuc"],
+        )
+        DataFrames.append!(df_objs, DataFrames.DataFrame(objectives))
+    end
     CSV.write(
-        paths["outputs"]["obj_no_water_weights"],
-        DataFrames.DataFrame(objectives)
+        paths["outputs"]["objectives"],
+        df_objs
     )
-    df_gen_states = MOCOT.pm_state_df(state["power"], "gen", ["pg"])
-    CSV.write(paths["outputs"]["no_water_weights"], df_gen_states)
 
-    # Simulation with no water weights
-    (objectives, state) = MOCOT.simulation(
-        network_data,
-        df_gen_info, 
-        df_eia_heat_rates, 
-        df_air_water,
-        df_node_load,
-        w_with=1.0,
-        w_con=0.0,
-    )
-    CSV.write(
-        paths["outputs"]["obj_with_water_weights"],
-        DataFrames.DataFrame(objectives)
-    )
-    df_gen_states = MOCOT.pm_state_df(state["power"], "gen", ["pg"])
-    CSV.write(paths["outputs"]["with_water_weights"], df_gen_states)
+    # df_gen_states = MOCOT.pm_state_df(state["power"], "gen", ["pg"])
+    # CSV.write(paths["outputs"]["no_water_weights"], df_gen_states)
 
     # Generator information export
     CSV.write(paths["outputs"]["gen_info_main"], df_gen_info)
