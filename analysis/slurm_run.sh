b9727421f6f0eb53b8083f619937901cf1809d6f
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-09-13 10:31:15 -0600

    Add slurm run file

--- analysis/slurm_run.sh
+++ analysis/slurm_run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
 #SBATCH --output=run.out
 #SBATCH --job-name=gosox
 #SBATCH --partition=amilan
-#SBATCH --time=0-00:03:00
+#SBATCH --time=0-00:600:00
 #SBATCH --mail-user=jacob.kravits@colorado.edu
 #SBATCH --mail-type=END
 
 # Setup
 module purge
+export JULIA_DEPOT_PATH="/projects/jakr3868/.julia:$JULIA_DEPOT_PATH"
 ml julia/1.6.6
 
 # Run analysis
