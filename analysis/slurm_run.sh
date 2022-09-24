39ed301959ef1e4dcf785ed0efa9742122cf6c2d
kravitsjacob <kravitsjacob@gmail.com>
2022-09-23 23:53:19 -0600

    Use default outputs in slurm

--- analysis/slurm_run.sh
+++ analysis/slurm_run.sh
@@ -1,13 +1,12 @@
+#!/bin/sh
 
-#SBATCH --ntasks=144
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=1
 #SBATCH --switches=1
 #SBATCH --constraint=ib
-#SBATCH --output=output.txt
-#SBATCH --error=error.txt
 #SBATCH --job-name=gosox
 #SBATCH --partition=amilan
-#SBATCH --time=0-00:10:00
+#SBATCH --time=0-00:20:00
 #SBATCH --mail-user=jakr3868@colorado.edu
 #SBATCH --mail-type=ALL
 
@@ -17,4 +16,4 @@ cd ..
 . analysis/slurm_config.sh
 
 # Run analysis
-mpiexec -n 144 ./analysis/main.exe
+mpiexec -n 4 ./analysis/optimization.exe $scenario_code
