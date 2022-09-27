1506d36a9305ae94847fc6a8820226804a835d8d
kravitsjacob <kravitsjacob@gmail.com>
2022-09-27 17:17:04 -0600

    Update slurm requests

--- analysis/slurm_run.sh
+++ analysis/slurm_run.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-#SBATCH --ntasks=4
+#SBATCH --ntasks=144
 #SBATCH --cpus-per-task=1
 #SBATCH --switches=1
 #SBATCH --constraint=ib
 #SBATCH --job-name=gosox
 #SBATCH --partition=amilan
-#SBATCH --time=0-00:20:00
+#SBATCH --time=0-00:60:00
 #SBATCH --mail-user=jakr3868@colorado.edu
 #SBATCH --mail-type=ALL
 
 # Setup
 module purge
-cd ..
 . analysis/slurm_config.sh
 
 # Run analysis
-mpiexec -n 4 ./analysis/optimization.exe $scenario_code
+mpiexec -n 144 ./analysis/optimization.exe $scenario_code
