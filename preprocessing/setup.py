96e9095e1f20e982853f3731232235b24ef7585f
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-09-29 13:58:42 -0600

    Preprocessing moved to own directory

---
+++ preprocessing/setup.py
@@ -0,0 +1,31 @@
+
+import setuptools
+
+setuptools.setup(
+    name="premocot",
+    version="0.0.14b0",
+    author="Jacob Kravits",
+    author_email="kravitsjacob@gmail.com",
+    description="Multi-Objective Coordination of Thermoelectric Water Use",
+    url="https://github.com/kravitsjacob/water-coordinate",
+    project_urls={
+        "Bug Tracker": "https://github.com/kravitsjacob/water-coordinate/issues",  # noqa
+    },
+    install_requires=[
+        'pandas',
+        'tables',
+        'openpyxl',
+        'pandapower >= 2.10.1',
+        'PyYAML',
+        'matpowercaseframes'
+        # 'pygmo' TODO port project, conda install -c conda-forge pygmo
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+)
