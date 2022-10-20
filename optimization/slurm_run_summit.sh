6de088cb2c8200add1ded355f33833680547b7a6
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-10-20 16:20:27 -0600

    Update summit runs

--- optimization/slurm_run_summit.sh
+++ optimization/slurm_run_summit.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-#SBATCH --nodes=18
+#SBATCH --nodes=36
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=1
+#SBATCH --switches=1
 #SBATCH --job-name=gosox
 #SBATCH --partition=shas
 #SBATCH --qos=condo
 #SBATCH --account=ucb-summit-jrk
-#SBATCH --time=0-00:200:00
+#SBATCH --time=0-00:10:00
 #SBATCH --mail-user=jakr3868@colorado.edu
 #SBATCH --mail-type=ALL
 
 # Setup
 module purge
-. optimization/slurm_config.sh
+. optimization/slurm_config_summit.sh
 
 # Run analysis
-mpiexec -n 72 ./optimization/optimization.exe ${SLURM_ARRAY_TASK_ID}
+mpiexec -n 144 ./optimization/optimization.exe ${SLURM_ARRAY_TASK_ID}
