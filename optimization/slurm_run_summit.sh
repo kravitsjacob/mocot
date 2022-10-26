93c73ff872950b844d55a306fd7f5345b13d8899
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-10-26 06:17:47 -0600

    Updated results

--- optimization/slurm_run_summit.sh
+++ optimization/slurm_run_summit.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#SBATCH --nodes=9
-#SBATCH --ntasks-per-node=8
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=1
 #SBATCH --switches=1
 #SBATCH --job-name=gosox
 #SBATCH --partition=shas
 #SBATCH --qos=condo
 #SBATCH --account=ucb-summit-jrk
-#SBATCH --time=0-00:250:00
+#SBATCH --time=0-00:300:00
 #SBATCH --mail-user=jakr3868@colorado.edu
 #SBATCH --mail-type=ALL
 
